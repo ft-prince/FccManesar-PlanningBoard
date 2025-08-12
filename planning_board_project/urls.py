51d1b670fdc55eeef43be1916cdc1141dbea2307
ft-prince <ft.princeee@gmail.com>
2025-08-12 14:37:19 +0530

    changes in urls

--- planning_board_project/urls.py
+++ planning_board_project/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from planning_board import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('planning/', include('planning_board.urls')),
+    path('', views.planning_board_dashboard, name='dashboard'),
     # Add other app URLs here
 ]
